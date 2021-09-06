9de7577523a0ae82b16b13256a8b4dcdec0bb398
GyozaGuy <gyozaguy@gmail.com>
2021-09-06 11:59:21 -0600

    Export render

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { html } from 'uhtml';
+export { html, render } from 'uhtml';
 export { default as attrToPropName } from './src/attrToPropName';
 export { Component, define } from './src/Component';
 export { default as propToAttrName } from './src/propToAttrName';
